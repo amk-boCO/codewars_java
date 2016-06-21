533c93747ad8d4ed0110f7b73df2db9a83401fed
Andreas M. Kavountzis <akavount@gmail.com>
2016-06-21 14:41:05 -0600

    Improve solution to Persist

--- src/main/java/Persist.java
+++ src/main/java/Persist.java
@@ -1,20 +1,18 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Persist {
     static int persistence(int num) {
         if (num < 10) return 0;
 
-        String numStr = String.valueOf(num);
-        List<String> characters = new ArrayList<>(Arrays.asList(numStr.split("")));
-
-        int product = characters.size() > 1 ? 1 : 0;
-
-        for (String character : characters) {
-            product *= Integer.parseInt(character);
-        }
+        String[] digitString = String.valueOf(num).split("");
+        int product = product(digitString);
 
         return product < 10 ? 1 : 1 + persistence(product);
+    }
+
+    private static int product(String[] numStr) {
+        return Arrays.asList(numStr).stream().
+            mapToInt(Integer::parseInt).
+            reduce(1, Math::multiplyExact);
     }
 }
